026b4438376077f775932604bc74bb763ca41f95
INDIGO-VAIO <INDIGO-VAIO>
2016-11-17 23:41:16 +0500

    add express server

--- src/js/script.js
+++ src/js/script.js
@@ -1,18 +1,5 @@
 var map;
 
-/**
- * Overwrites obj1's values with obj2's and adds obj2's if non existent in obj1
- * @param obj1
- * @param obj2
- * @returns obj3 a new object based on obj1 and obj2
- */
-function merge_options(obj1,obj2){
-    var obj3 = {};
-    for (var attrname in obj1) { obj3[attrname] = obj1[attrname]; }
-    for (var attrname in obj2) { obj3[attrname] = obj2[attrname]; }
-    return obj3;
-}
-
 function initMap() 
 {
   map = new google.maps.Map(document.getElementById('map'), {
@@ -48,30 +35,11 @@ function initDrawing(map)
     markerOptions: {
       draggable: true,
       editable: true,
+      title: 'test'
     }
   };
 
-  var drawingManager = new google.maps.drawing.DrawingManager({
-    // drawingMode: google.maps.drawing.OverlayType.MARKER,     // Default draw tool    
-    // drawingControl: false                                    // Hide control
-    drawingControlOptions: {
-      position: google.maps.ControlPosition.TOP_CENTER,
-      drawingModes: [
-        google.maps.drawing.OverlayType.MARKER,
-        google.maps.drawing.OverlayType.POLYLINE,
-        google.maps.drawing.OverlayType.POLYGON,
-        google.maps.drawing.OverlayType.CIRCLE,
-        google.maps.drawing.OverlayType.RECTANGLE
-      ]
-    },
-    circleOptions: {
-      fillColor: '#ffff00',
-      fillOpacity: 0.3,
-      strokeWeight: 2,
-      clickable: false,
-      editable: true,
-    }
-  });
+  var drawingManager = new google.maps.drawing.DrawingManager(drawingManagerOptions);
   drawingManager.setMap(map);
 
   var drawing = {
@@ -107,14 +75,6 @@ function createControlPosition(drawing)
   // handlers
   //
   controlPositionSelect.change(function(){  
-    var newDrawingControlOptions = {
-      drawingControlOptions: {
-        position: google.maps.ControlPosition[$(this).val()]
-      }
-    }
-
-    var mergeDrawingControlOptions = merge_options(drawing.config, newDrawingControlOptions);
-
-    drawing.manager.setOptions(mergeDrawingControlOptions);
+    
   });
 }
