27a18128f66cb774226855070222d4d0a74d2676
INDIGO-VAIO <INDIGO-VAIO>
2016-11-22 01:09:09 +0500

    add complite draw handler

--- src/js/script.js
+++ src/js/script.js
@@ -1,32 +1,33 @@
 var map;
+var drawingManager;
+var infoWindow;
+var store;
+
+// functions
 
 function initMap() 
 {
-  var map = new google.maps.Map(document.getElementById('map'), {
+  map = new google.maps.Map(document.getElementById('map'), {
     center: {lat: -34.397, lng: 150.644},
     zoom: 8
   });
 
   var markerPosition = new google.maps.LatLng(-34.397, 150.644);
 
-  var marker = new google.maps.Marker({
-    position: markerPosition,
-    map: map
-  });
-
   // marker.setMap(map);
 
   initDrawing(map);
 }
 
-function initDrawing(map)
+function initDrawing()
 {
-  var drawingManagerOptions = {
-    // drawingMode: google.maps.drawing.OverlayType.MARKER,     // Default draw tool    
-    // drawingControl: false                                    // Hide control
-    drawingControlOptions: {
+  var drawingManagerOptions = 
+  {
+    drawingControlOptions: 
+    {
       position: google.maps.ControlPosition.TOP_CENTER,
-      drawingModes: [
+      drawingModes: 
+      [
         google.maps.drawing.OverlayType.MARKER,
         google.maps.drawing.OverlayType.POLYLINE,
         google.maps.drawing.OverlayType.POLYGON,
@@ -34,55 +35,57 @@ function initDrawing(map)
         google.maps.drawing.OverlayType.RECTANGLE
       ]
     },
-    circleOptions: {
-      fillColor: '#ffff00',
-      fillOpacity: 0.3,
-      strokeWeight: 2,
-      clickable: false,
-      editable: true,
+    circleOptions: 
+    {
+      editable: true
     },
-    markerOptions: {
-      draggable: true,
-      title: 'Test'
+    markerOptions: 
+    {
+      draggable: true
     }
   };
 
-  var drawingManager = new google.maps.drawing.DrawingManager(drawingManagerOptions);
+  drawingManager = new google.maps.drawing.DrawingManager(drawingManagerOptions);
+
   drawingManager.setMap(map);
 
-  var drawing = {
-    config: drawingManagerOptions,
-    manager: drawingManager
-  }
-
-  initDrawConfigPanel(drawing);
-}
-
-function initDrawConfigPanel(drawing)
-{
-  createControlPosition(drawing);
-}
-
-function createControlPosition(drawing)
-{
-  var controlPositionOptions = '';
-
-  //
-  // create
-  //
-  for(position in google.maps.ControlPosition)
-  {
-    controlPositionOptions += '<option value="' + position+ '">' + position+ '</option>';
-  }
-
-  var controlPositionSelect = $('<select>', {id: 'control-position'})
-    .append(controlPositionOptions)
-    .appendTo('#map-control');
-
-  //
   // handlers
-  //
-  controlPositionSelect.change(function(){  
-    
+
+  google.maps.event.addListener(drawingManager, 'overlaycomplete', function(event) 
+  {
+    switch(event.type)
+    {
+      case google.maps.drawing.OverlayType.MARKER:
+        var coordinates = event.overlay.getPosition().toJSON();
+        var InfoWindowContent = prompt('Укажите текст подсказки', '');
+        setInfoWindow(map, event.overlay, InfoWindowContent);
+        break;
+      case google.maps.drawing.OverlayType.POLYLINE:
+        break;
+      case google.maps.drawing.OverlayType.POLYGON:
+        break;
+      case google.maps.drawing.OverlayType.CIRCLE:
+        break;
+      case google.maps.drawing.OverlayType.RECTANGLE:
+        break; 
+    }
   });
+
+  // functions
+
+  function setInfoWindow(map, marker, content)
+  {
+    if(content)
+    {
+      google.maps.event.addListener(marker, 'click', function()
+      {
+        if(typeof infoWindow != 'undefined') infoWindow.close(); 
+        infoWindow = new google.maps.InfoWindow({
+          content: content,
+          maxWidth: 200
+        });
+        infoWindow.open(map, marker);
+      });
+    }
+  }
 }
